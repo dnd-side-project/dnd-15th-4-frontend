b13d5ab00e974bc301b9fd7eb764c9651cd7cf4c
nakyeong <yunna3425@naver.com>
2026-08-08 04:20:07 +0900

    feat: 2구 버튼 컴포넌트 구현 (#3)

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -4,11 +4,12 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 export const buttonVariants = cva(
-  "group/button inline-flex shrink-0 items-center justify-center border border-transparent bg-clip-padding font-medium whitespace-nowrap transition-all outline-none select-none focus-visible:border-sub2-normal focus-visible:ring-3 focus-visible:ring-sub2-normal/50 disabled:pointer-events-none disabled:opacity-50",
+  "group/button inline-flex shrink-0 items-center justify-center border border-transparent bg-clip-padding font-medium whitespace-nowrap transition-all outline-none select-none focus-visible:border-sub2-normal focus-visible:ring-3 focus-visible:ring-sub2-normal/50 disabled:pointer-events-none disabled:bg-disable",
   {
     variants: {
       variant: {
         default: "bg-sub2-normal text-white",
+        secondary: "bg-white text-primary border-border-1",
       },
       size: {
         default: "bottom-button w-full h-14 rounded-16",
@@ -21,12 +22,15 @@ export const buttonVariants = cva(
   }
 );
 
+export interface ButtonProps
+  extends ButtonPrimitive.Props, VariantProps<typeof buttonVariants> {}
+
 export const Button = ({
   className,
   variant = "default",
   size = "default",
   ...props
-}: ButtonPrimitive.Props & VariantProps<typeof buttonVariants>) => {
+}: ButtonProps) => {
   return (
     <ButtonPrimitive
       data-slot="button"
