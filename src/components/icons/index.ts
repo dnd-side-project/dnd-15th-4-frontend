dd8790a023d50c6819ae08c87d146fcedcaf3e1b
조희우 <huiwoo.jo@gmail.com>
2026-08-12 17:02:15 +0900

    chore: 홈 화면에서 사용하는 아이콘 파일 추가 (#6)

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,4 +1,7 @@
 export * from "./BaseIcon";
 export * from "./IcExtension";
+export * from "./IcKakao";
+export * from "./IcPlus";
+export * from "./IcProfile";
 export * from "./IcViewAgenda";
 export * from "./icon.types";
