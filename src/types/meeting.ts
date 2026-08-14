013bca7b044ec78c5fd036c6fd445927c0d26f24
조희우 <huiwoo.jo@gmail.com>
2026-08-14 14:11:46 +0900

    feat: 스케쥴 데이터의 데이터 타입 및 목업 데이터 추가 (#6)

---
+++ src/types/meeting.ts
@@ -0,0 +1,19 @@
+export interface Participant {
+  id: number;
+  name: string;
+  profileImageNumber: number;
+}
+
+export interface MeetingData {
+  meetingId: number;
+  title: string;
+  dateTime: string;
+  place: string;
+  latitude: number;
+  longitude: number;
+  participants: Participant[];
+}
+
+export interface MeetingListResponse {
+  data: MeetingData[];
+}
