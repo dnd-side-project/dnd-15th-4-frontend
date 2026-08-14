f44d963b5cb5fc5971ee5f442c5dad0ee8869650
조희우 <huiwoo.jo@gmail.com>
2026-08-14 14:14:48 +0900

    fix: 진행중인 약속이 없을 경우의 홈 hero 부분의 이미지를 원본으로 수정 (#6)

--- src/components/home/hero/HomeHeroSection.tsx
+++ src/components/home/hero/HomeHeroSection.tsx
@@ -5,7 +5,7 @@ import { ProfileMenu } from "@/components/home/ProfileMenu";
 
 export const HomeHeroSection = () => {
   return (
-    <section className="bg-primary-light-active relative h-[54dvh] max-h-125 min-h-100 w-full overflow-hidden">
+    <section className="bg-primary-light-active relative h-[54dvh] max-h-80 min-h-100 w-full overflow-hidden">
       <div className="absolute top-11 right-4 z-20">
         <ProfileMenu />
       </div>
