0dc04af26332b75254916d1b546c91dcfa0ca318
nakyeong <yunna3425@naver.com>
2026-08-16 23:35:44 +0900

    feat: 약속방 진행 중 화면에 사용할 아이콘 추가 (#8)

---
+++ src/components/icons/IcArrowOutward.tsx
@@ -0,0 +1,8 @@
+import { BaseIcon } from "./BaseIcon";
+import type { IconProps } from "./icon.types";
+
+export const IcArrowOutward = (props: IconProps) => (
+  <BaseIcon {...props}>
+    <path d="M6.29449 17.6443L5.25024 16.6L15.0907 6.75001H6.14449V5.25001H17.6445V16.75H16.1445V7.80376L6.29449 17.6443Z" />
+  </BaseIcon>
+);
