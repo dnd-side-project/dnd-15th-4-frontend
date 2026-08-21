c0b2be0336be0c4ef8620880d9a42520f2f6f5a4
조희우 <huiwoo.jo@gmail.com>
2026-08-22 01:48:48 +0900

    rename: 카카오톡 로그인 버튼 파일 위치 수정 및 디자인 반영 (#10)

--- src/components/home/KakaoLoginButton.tsx
+++ src/components/home/KakaoLoginButton.tsx
@@ -1,14 +1,9 @@
 import { IcKakao } from "@/components/icons";
 
-export interface KakaoLoginButtonProps {
-  onClick?: () => void;
-}
-
-export const KakaoLoginButton = ({ onClick }: KakaoLoginButtonProps) => (
+export const KakaoLoginButton = () => (
   <div className="flex w-full flex-col items-center gap-2">
     <button
       type="button"
-      onClick={onClick}
       className="rounded-16 bg-kakao flex h-14 w-full items-center justify-center gap-2"
     >
       <IcKakao size={24} />
