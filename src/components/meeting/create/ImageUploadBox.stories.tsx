4d22e5f98ce10a16d186bab112caacba815949fe
조희우 <huiwoo.jo@gmail.com>
2026-08-22 19:33:20 +0900

    fix: 스토리북 타입 에러로 인한 ci/cd 충돌 해결 (#10)

--- src/components/meeting/create/ImageUploadBox.stories.tsx
+++ src/components/meeting/create/ImageUploadBox.stories.tsx
@@ -40,13 +40,6 @@ export const WithDefaultImage: Story = {
   },
 };
 
-export const RequiredError: Story = {
-  args: {
-    selectedImage: null,
-    errorMessage: "이미지를 선택해 주세요",
-  },
-};
-
 export const Interactive: Story = {
   render: () => {
     const [selectedImage, setSelectedImage] =
