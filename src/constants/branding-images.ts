5abdf6b124f88dd5be3a75a070d8e97906f98ee3
조희우 <huiwoo.jo@gmail.com>
2026-08-22 13:29:52 +0900

    chore: 기본 퍼즐 및 프로필 이미지 파일 수정 및 추가 (#10)

--- src/constants/branding-images.ts
+++ src/constants/branding-images.ts
@@ -10,6 +10,18 @@ import branding7 from "@/assets/images/branding/branding-7.png";
 import branding8 from "@/assets/images/branding/branding-8.png";
 import branding9 from "@/assets/images/branding/branding-9.png";
 import branding10 from "@/assets/images/branding/branding-10.png";
+import branding11 from "@/assets/images/branding/branding-11.png";
+import branding12 from "@/assets/images/branding/branding-12.png";
+import branding13 from "@/assets/images/branding/branding-13.png";
+import branding14 from "@/assets/images/branding/branding-14.png";
+import branding15 from "@/assets/images/branding/branding-15.png";
+import branding16 from "@/assets/images/branding/branding-16.png";
+import branding17 from "@/assets/images/branding/branding-17.png";
+import branding18 from "@/assets/images/branding/branding-18.png";
+import branding19 from "@/assets/images/branding/branding-19.png";
+import branding20 from "@/assets/images/branding/branding-20.png";
+import branding21 from "@/assets/images/branding/branding-21.png";
+import branding22 from "@/assets/images/branding/branding-22.png";
 
 export const BRAND_IMAGES: StaticImageData[] = [
   branding1,
@@ -22,6 +34,18 @@ export const BRAND_IMAGES: StaticImageData[] = [
   branding8,
   branding9,
   branding10,
+  branding11,
+  branding12,
+  branding13,
+  branding14,
+  branding15,
+  branding16,
+  branding17,
+  branding18,
+  branding19,
+  branding20,
+  branding21,
+  branding22,
 ];
 
 export const getRandomBrandImage = (): StaticImageData =>
