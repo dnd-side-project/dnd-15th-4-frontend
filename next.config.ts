e2d917dab47a718ee3507e29f154486a26aa1218
조희우 <huiwoo.jo@gmail.com>
2026-08-23 18:35:25 +0900

    feat: 이미지 호출 및 사용 config 설정 (#15)

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: process.cwd(),
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "puzzle-meet-s3.s3.ap-northeast-2.amazonaws.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
