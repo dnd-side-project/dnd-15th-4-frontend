35eb01b8ca5f8494df64f4a71c8db3c1be804ea3
nakyeong <yunna3425@naver.com>
2026-08-29 09:09:16 +0900

    chore: 카카오 CDN 도메인 추가 (#80)

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,22 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "puzzle-meet-s3.s3.ap-northeast-2.amazonaws.com",
       },
+      {
+        protocol: "http",
+        hostname: "k.kakaocdn.net",
+      },
+      {
+        protocol: "https",
+        hostname: "k.kakaocdn.net",
+      },
+      {
+        protocol: "http",
+        hostname: "*.kakaocdn.net",
+      },
+      {
+        protocol: "https",
+        hostname: "*.kakaocdn.net",
+      },
     ],
   },
 };
